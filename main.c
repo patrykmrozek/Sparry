9ab158586c61d35817d1ff1adcbc4baf988b16a1
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-05-24 18:53:58 +0100

    mat4 - some linalg

--- main.c
+++ main.c
@@ -14,13 +14,32 @@ static f32 zbuffer[SCREEN_WIDTH*SCREEN_HEIGHT];
 
 typedef struct vec3_s {
     f32 x,y,z;
-} vec3;
+} vec3_t;
 
 typedef struct vec4_s {
     f32 x,y,z,w;
-} vec4;
+} vec4_t;
 
-void put_pixel(u32 x, u32 y, u32 z, u32 c)
+typedef struct mat4_s {
+    vec4_t v[4];
+} mat4_t;
+
+static inline f32 vec3_dot(vec3_t v1, vec3_t v2)
+{
+    return ((v1.x*v2.x) + (v1.y*v2.y) + (v1.z*v2.z));
+}
+
+static inline vec4_t mat4_mul_vec4(mat4_t m, vec4_t v)
+{
+    return (vec4_t){
+        vec3_dot((vec3_t){v.x, v.y, v.z}, (vec3_t){m.v[0].x, m.v[1].x, m.v[2].x}) + m.v[3].x,
+        vec3_dot((vec3_t){v.x, v.y, v.z}, (vec3_t){m.v[0].y, m.v[1].y, m.v[2].y}) + m.v[3].y,
+        vec3_dot((vec3_t){v.x, v.y, v.z}, (vec3_t){m.v[0].z, m.v[1].z, m.v[2].z}) + m.v[3].z,
+        1,
+    };
+}
+
+static inline void put_pixel(u32 x, u32 y, u32 z, u32 c)
 {
     u32 idx = (y * SCREEN_WIDTH) + x;
     if (!zbuffer[idx] || zbuffer[idx]> z) {
@@ -29,7 +48,7 @@ void put_pixel(u32 x, u32 y, u32 z, u32 c)
     }
 }
 
-void put_pixel_vec(vec3 v, u32 c)
+static inline void put_pixel_vec(vec3_t v, u32 c)
 {
     u32 idx = (v.y * SCREEN_WIDTH) + v.x;
     if (!zbuffer[idx] || zbuffer[idx]> v.z) {
@@ -61,6 +80,16 @@ int main()
     SDL_Event event;
     bool game_running = 1;
 
+    vec4_t v = (vec4_t){1, 1, 1, 1};
+    mat4_t m = (mat4_t){
+        (vec4_t){2, 0, 0, 0},
+        (vec4_t){0, 2, 0, 0},
+        (vec4_t){0, 0, 2, 0},
+        (vec4_t){1, 2, 3, 1},
+    };
+    vec4_t mv = mat4_mul_vec4(m, v);
+    printf("mv: {%f,%f,%f}\n", mv.x, mv.y, mv.z);
+
     while (game_running == 1) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
@@ -72,9 +101,9 @@ int main()
             framebuffer[10000+i] = 0xFFFF0000;
             put_pixel((SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+i, 5, 0xFF00FF00);
             put_pixel((SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+100-i, 10, 0xFF0000FF);
-            put_pixel_vec((vec3){(SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+50, 7}, 0xFFFFFF00);
+            put_pixel_vec((vec3_t){(SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+50, 7}, 0xFFFFFF00);
         }
-
+        
         SDL_UpdateTexture(texture, NULL, framebuffer, SCREEN_WIDTH * sizeof(u32));
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
