6e2586f3f876886101730a21ceac40b512a53e19
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-05-27 22:38:24 +0100

    rename types, add more v4 funcs

--- main.c
+++ main.c
@@ -12,56 +12,88 @@ typedef float f32;
 static u32 framebuffer[SCREEN_WIDTH*SCREEN_HEIGHT];
 static f32 zbuffer[SCREEN_WIDTH*SCREEN_HEIGHT];
 
-typedef struct vec3_s {
+typedef struct v3_s {
     f32 x,y,z;
-} vec3_t;
-
-typedef struct vec4_s {
-    f32 x,y,z,w;
-} vec4_t;
-
-typedef struct mat4_s {
-    vec4_t v[4];
-} mat4_t;
-
-static inline vec3_t vec3_add(vec3_t v1, vec3_t v2)
+} v3;
+static inline v3 v3_add(v3 v1, v3 v2)
 {
-    return (vec3_t){v1.x+v2.x, v1.y+v2.y, v1.z+v2.z};
+    return (v3){v1.x+v2.x, v1.y+v2.y, v1.z+v2.z};
 }
-static inline vec3_t vec3_sub(vec3_t v1, vec3_t v2)
+static inline v3 v3_sub(v3 v1, v3 v2)
 {
-    return (vec3_t){v1.x-v2.x, v1.y-v2.y, v1.z-v2.z};
+    return (v3){v1.x-v2.x, v1.y-v2.y, v1.z-v2.z};
 }
-static inline vec3_t vec3_mul(vec3_t v1, vec3_t v2)
+static inline v3 v3_scale(v3 v, f32 s)
 {
-   return (vec3_t){v1.x*v2.x, v1.y*v2.y, v1.z*v2.z}; 
+    return (v3){v.x*s, v.y*s, v.z*s};
 }
-static inline vec3_t vec3_div(vec3_t v1, vec3_t v2)
+static inline v3 v3_mul(v3 v1, v3 v2)
 {
-    return (vec3_t){v1.x/v2.x, v1.y/v2.y, v1.z/v2.z};
+   return (v3){v1.x*v2.x, v1.y*v2.y, v1.z*v2.z}; 
 }
-static inline f32 vec3_dot(vec3_t v1, vec3_t v2)
+static inline v3 v3_div(v3 v1, v3 v2)
+{
+    return (v3){v1.x/v2.x, v1.y/v2.y, v1.z/v2.z};
+}
+static inline f32 v3_dot(v3 v1, v3 v2)
 {
     return ((v1.x*v2.x) + (v1.y*v2.y) + (v1.z*v2.z));
 }
-static inline f32 vec3_length(vec3_t v)
+static inline f32 v3_len(v3 v)
 {
-    return (sqrtf(vec3_dot(v, v)));
+    return (sqrtf(v3_dot(v, v)));
 }
-static inline vec3_t vec3_normalize(vec3_t v)
+static inline v3 v3_norm(v3 v)
 {
-    f32 len = vec3_length(v);
-    return (vec3_t)vec3_div(v, (vec3_t){len, len, len});
+    f32 l = v3_len(v);
+    return (v3)v3_div(v, (v3){l, l, l});
 }
-static inline vec4_t mat4_mul_vec4(mat4_t m, vec4_t v)
+
+typedef struct v4_s {
+    f32 x,y,z,w;
+} v4;
+typedef struct m4_s {
+    v4 v[4];
+} m4;
+static inline v4 v4_norm(v4 v)
 {
-    return (vec4_t){
-        vec3_dot((vec3_t){v.x, v.y, v.z}, (vec3_t){m.v[0].x, m.v[1].x, m.v[2].x}) + m.v[3].x,
-        vec3_dot((vec3_t){v.x, v.y, v.z}, (vec3_t){m.v[0].y, m.v[1].y, m.v[2].y}) + m.v[3].y,
-        vec3_dot((vec3_t){v.x, v.y, v.z}, (vec3_t){m.v[0].z, m.v[1].z, m.v[2].z}) + m.v[3].z,
+    return (v4){v.x/v.w, v.y/v.w, v.z/v.w, v.w/v.w}; 
+}
+static inline v4 m4_v4_mul(m4 m, v4 v)
+{
+    return (v4){
+        v3_dot((v3){v.x, v.y, v.z}, (v3){m.v[0].x, m.v[1].x, m.v[2].x}) + m.v[3].x,
+        v3_dot((v3){v.x, v.y, v.z}, (v3){m.v[0].y, m.v[1].y, m.v[2].y}) + m.v[3].y,
+        v3_dot((v3){v.x, v.y, v.z}, (v3){m.v[0].z, m.v[1].z, m.v[2].z}) + m.v[3].z,
         1,
     };
 }
+static inline v4 v4_trans(v4 v, v3 t)
+{
+    m4 tm = (m4){
+        (v4){1,       0,       0,       0},
+        (v4){0,       1,       0,       0},
+        (v4){0,       0,       1,       0},
+        (v4){t.x*v.x, t.y*v.y, t.z*v.z, 1},
+    };
+    return m4_v4_mul(tm, v);
+}
+static inline v4 v4_scale(v4 v, f32 s)
+{
+    m4 sm = (m4){
+        (v4){v.x*s, 0,     0,     0},
+        (v4){0,     v.y*s, 0,     0},
+        (v4){0,     0,     v.z*s, 0},
+        (v4){0,     0,     0,     1},
+    };
+    return m4_v4_mul(sm, v);
+}
+
+typedef struct camera_s {
+    v3 pos;
+    v3 up;
+    v3 look_at;
+} camera_t;
 
 static inline void put_pixel(u32 x, u32 y, u32 z, u32 c)
 {
@@ -72,7 +104,7 @@ static inline void put_pixel(u32 x, u32 y, u32 z, u32 c)
     }
 }
 
-static inline void put_pixel_vec(vec3_t v, u32 c)
+static inline void put_pixel_vec(v3 v, u32 c)
 {
     u32 idx = (v.y * SCREEN_WIDTH) + v.x;
     if (!zbuffer[idx] || zbuffer[idx]> v.z) {
@@ -104,14 +136,14 @@ int main()
     SDL_Event event;
     bool game_running = 1;
 
-    vec4_t v = (vec4_t){1, 1, 1, 1};
-    mat4_t m = (mat4_t){
-        (vec4_t){2, 0, 0, 0},
-        (vec4_t){0, 2, 0, 0},
-        (vec4_t){0, 0, 2, 0},
-        (vec4_t){1, 2, 3, 1},
+    v4 v = (v4){1, 1, 1, 1};
+    m4 m = (m4){
+        (v4){2, 0, 0, 0},
+        (v4){0, 2, 0, 0},
+        (v4){0, 0, 2, 0},
+        (v4){1, 2, 3, 1},
     };
-    vec4_t mv = mat4_mul_vec4(m, v);
+    v4 mv = m4_v4_mul(m, v);
     printf("mv: {%f,%f,%f}\n", mv.x, mv.y, mv.z);
 
     while (game_running == 1) {
@@ -125,7 +157,7 @@ int main()
             framebuffer[10000+i] = 0xFFFF0000;
             put_pixel((SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+i, 5, 0xFF00FF00);
             put_pixel((SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+100-i, 10, 0xFF0000FF);
-            put_pixel_vec((vec3_t){(SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+50, 7}, 0xFFFFFF00);
+            put_pixel_vec((v3){(SCREEN_WIDTH/2)+i, (SCREEN_HEIGHT/2)+50, 7}, 0xFFFFFF00);
         }
         
         SDL_UpdateTexture(texture, NULL, framebuffer, SCREEN_WIDTH * sizeof(u32));
