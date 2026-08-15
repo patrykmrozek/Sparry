362e0e92a2e99122abb111379decbadda1bfd402
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-08-15 12:48:42 +0100

    update rd header

--- src/render.h
+++ src/render.h
@@ -12,10 +12,10 @@ typedef struct rd_state_s {
     rt_ctx_t *rt_ctx;
 } rd_state_t;
 
-result_t rd_state_init(rd_state_t **out_rd_state); 
-void rd_state_update(rd_state_t *rd_state);
-void rd_state_destroy(rd_state_t *rd_state);
-void rd_frame_begin(rd_state_t *rd_state);
-void rd_frame_end(rd_state_t *rd_state);
+result_t rd_state_init(rd_state_t **state_pp); 
+void rd_state_update(rd_state_t *state);
+void rd_state_destroy(rd_state_t *state);
+void rd_frame_begin(rd_state_t *state);
+void rd_frame_end(rd_state_t *state);
 
 #endif //_RENDER_H
