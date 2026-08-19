7e7b036a1ff3cc1b979be05366123dd559561278
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-08-19 13:52:13 +0100

    update input handling

--- src/input.c
+++ src/input.c
@@ -3,31 +3,50 @@
 
 #include <SDL2/SDL.h>
 
-f32 delta_move = 0.3f;
-f32 delta_look = 0.7f;
+// TODO: update to frame independant dtime
+#define DELTA_MOVE 0.3f
+#define DELTA_LOOK 0.3f
+
+#define PITCH_LIMIT_DEG 85.0f
+#define PITCH_LIMIT_RAD  DEG_TO_RAD(PITCH_LIMIT_DEG)
+
+static f32 pitch = 0.0f;
+static f32 yaw = 0.0f;
 
 void input_process(const uint8_t* keystate)
 {
-    if (keystate[SDL_SCANCODE_W]) 
-        { g_camera.pos.z += delta_move; g_camera.look_at.z += delta_move; }
-    if (keystate[SDL_SCANCODE_S]) 
-        { g_camera.pos.z -= delta_move; g_camera.look_at.z -= delta_move; }
-    if (keystate[SDL_SCANCODE_A]) 
-        { g_camera.pos.x -= delta_move; g_camera.look_at.x -= delta_move; }
-    if (keystate[SDL_SCANCODE_D]) 
-        { g_camera.pos.x += delta_move; g_camera.look_at.x += delta_move; }
-    if (keystate[SDL_SCANCODE_SPACE]) 
-        { g_camera.pos.y += delta_move; g_camera.look_at.y += delta_move; }
-    if (keystate[SDL_SCANCODE_LSHIFT]) 
-        { g_camera.pos.y -= delta_move; g_camera.look_at.y -= delta_move; }
-    if (keystate[SDL_SCANCODE_LEFT]) 
-        { g_camera.look_at.x -= delta_look; }
-    if (keystate[SDL_SCANCODE_RIGHT]) 
-        { g_camera.look_at.x += delta_look; }
-    if (keystate[SDL_SCANCODE_UP]) 
-        { g_camera.look_at.y += delta_look; }
-    if (keystate[SDL_SCANCODE_DOWN]) 
-        { g_camera.look_at.y -= delta_look; }
+    if (keystate[SDL_SCANCODE_LEFT])  { yaw -= DEG_TO_RAD(DELTA_LOOK); }
+    if (keystate[SDL_SCANCODE_RIGHT]) { yaw += DEG_TO_RAD(DELTA_LOOK); }
+    if (keystate[SDL_SCANCODE_UP])    { pitch += DEG_TO_RAD(DELTA_LOOK); }
+    if (keystate[SDL_SCANCODE_DOWN])  { pitch -= DEG_TO_RAD(DELTA_LOOK); }
+
+    if (pitch > PITCH_LIMIT_RAD)  pitch = PITCH_LIMIT_RAD;
+    if (pitch < -PITCH_LIMIT_RAD) pitch = -PITCH_LIMIT_RAD;
+
+    v3 dir = (v3){
+        .x = cosf(pitch) * sinf(yaw),
+        .y = sinf(pitch),
+        .z = cosf(pitch) * cosf(yaw),
+    };
+    v3 right = v3_norm(v3_cross(g_camera.up, dir));
+
+    if (keystate[SDL_SCANCODE_W]) {
+        g_camera.pos = v3_add(g_camera.pos, v3_scale(dir, DELTA_MOVE));
+    } 
+    if (keystate[SDL_SCANCODE_S]) {
+        g_camera.pos = v3_sub(g_camera.pos, v3_scale(dir, DELTA_MOVE));
+    } 
+    if (keystate[SDL_SCANCODE_A]) {
+        g_camera.pos = v3_sub(g_camera.pos, v3_scale(right, DELTA_MOVE));
+    } 
+    if (keystate[SDL_SCANCODE_D]) {
+        g_camera.pos = v3_add(g_camera.pos, v3_scale(right, DELTA_MOVE));
+    }
+    if (keystate[SDL_SCANCODE_SPACE])  g_camera.pos.y += DELTA_MOVE; 
+    if (keystate[SDL_SCANCODE_LSHIFT]) g_camera.pos.y -= DELTA_MOVE;
+
+    g_camera.look_at = v3_add(g_camera.pos, dir);
+
     if (keystate[SDL_SCANCODE_F1])
         {g_snapshot_mode = true;}
 }
