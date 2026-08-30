fa98a7ee7e9933daed21686b625bc03378d910c7
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-08-30 09:13:34 +0100

    add color macros

--- src/colour.h
+++ src/colour.h
@@ -5,7 +5,18 @@
 
 typedef struct colour_s {
     u8 r, g, b, a;
-} colour_t;
+#define COL_WHITE  (col_t){255, 255, 255, 255}
+#define COL_BLACK  (col_t){0,   0,   0,   0  }
+#define COL_RED    (col_t){255, 0,   0,   255}
+#define COL_GREEN  (col_t){0,   255, 0,   255}
+#define COL_BLUE   (col_t){0,   0,   255, 255}
+#define COL_YELLOW (col_t){255, 255, 0,   255}
+#define COL_PURPLE (col_t){255, 0,   255, 255}
+#define COL_LBLUE  (col_t){0,   255, 255, 255}
+#define COL_ORANGE (col_t){255, 200, 0,   255}
+#define COL_PINK   (col_t){255, 0,   150, 255}
+#define COL_LGREY  (col_t){150, 150, 150, 255}
+#define COL_DGREY  (col_t){50, 50, 50, 255}
 
 #define COL_TO_HEX(col) (u32)((col).a<<24)|\
                         (u32)((col).r<<16)|\
