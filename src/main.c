71f57caaf2b680b5df4df1f2c673b469229de86b
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-09-03 12:10:11 +0100

    wrap cube render into func

--- src/main.c
+++ src/main.c
@@ -12,6 +12,40 @@
 
 bool g_snapshot_mode = false;
 
+static inline void
+render_multicolor_cube(render_state_t *rdstate, f32 size)
+{
+    v3 bfl = {-size, -size,  size};
+    v3 bfr = { size, -size,  size};
+    v3 bcr = { size, -size, -size};
+    v3 bcl = {-size, -size, -size};
+
+    v3 tfl = {-size, size,  size};
+    v3 tfr = { size, size,  size};
+    v3 tcr = { size, size, -size};
+    v3 tcl = {-size, size, -size};
+
+    //front
+    raster_tri3c(rdstate->raster_ctx, tcl, bcl, bcr,
+                 COL_RED, COL_GREEN, COL_BLUE); 
+    raster_tri(rdstate->raster_ctx, tcl, tcr, bcr, COL_GREEN); 
+    //back
+    raster_tri(rdstate->raster_ctx, tfl, bfl, bfr, COL_BLUE); 
+    raster_tri(rdstate->raster_ctx, tfl, tfr, bfr, COL_YELLOW); 
+    //bottom
+    raster_tri(rdstate->raster_ctx, bfl, bcl, bcr, COL_PURPLE); 
+    raster_tri(rdstate->raster_ctx, bfl, bfr, bcr, COL_WHITE); 
+    //top
+    raster_tri(rdstate->raster_ctx, tfl, tcl, tcr, COL_BLACK); 
+    raster_tri(rdstate->raster_ctx, tfl, tfr, tcr, COL_LBLUE);
+    //left
+    raster_tri(rdstate->raster_ctx, tfl, bfl, bcl, COL_ORANGE); 
+    raster_tri(rdstate->raster_ctx, tfl, tcl, bcl, COL_PINK); 
+    //right
+    raster_tri(rdstate->raster_ctx, tfr, bfr, bcr, COL_LGREY); 
+    raster_tri(rdstate->raster_ctx, tfr, tcr, bcr, COL_DGREY);   
+}
+
 int main() 
 {
     INFO("SPARRY\n");
@@ -20,16 +54,6 @@ int main()
     result_t res = render_state_init(&render_state);
     HANDLE_ERROR_RET(res!=RESULT_OK, 1, NULL);
     
-    v3 bfl = {-10, -10,  10};
-    v3 bfr = { 10, -10,  10};
-    v3 bcr = { 10, -10, -10};
-    v3 bcl = {-10, -10, -10};
-
-    v3 tfl = {-10, 10,  10};
-    v3 tfr = { 10, 10,  10};
-    v3 tcr = { 10, 10, -10};
-    v3 tcl = {-10, 10, -10};
-
     v3 lp0 = {-20, 20, 20};
     v3 lp1 = { 20, -20, -20};
 
@@ -45,31 +69,15 @@ int main()
 
         g_snapshot_mode = false;
         input_process();
+
         render_frame_begin(render_state);
-        { 
-            //front
-            raster_tri3c(render_state->raster_ctx, tcl, bcl, bcr,
-                         COL_RED, COL_GREEN, COL_BLUE); 
-            raster_tri(render_state->raster_ctx, tcl, tcr, bcr, COL_GREEN); 
-            //back
-            raster_tri(render_state->raster_ctx, tfl, bfl, bfr, COL_BLUE); 
-            raster_tri(render_state->raster_ctx, tfl, tfr, bfr, COL_YELLOW); 
-            //bottom
-            raster_tri(render_state->raster_ctx, bfl, bcl, bcr, COL_PURPLE); 
-            raster_tri(render_state->raster_ctx, bfl, bfr, bcr, COL_WHITE); 
-            //top
-            raster_tri(render_state->raster_ctx, tfl, tcl, tcr, COL_BLACK); 
-            raster_tri(render_state->raster_ctx, tfl, tfr, tcr, COL_LBLUE);
-            //left
-            raster_tri(render_state->raster_ctx, tfl, bfl, bcl, COL_ORANGE); 
-            raster_tri(render_state->raster_ctx, tfl, tcl, bcl, COL_PINK); 
-            //right
-            raster_tri(render_state->raster_ctx, tfr, bfr, bcr, COL_LGREY); 
-            raster_tri(render_state->raster_ctx, tfr, tcr, bcr, COL_DGREY);  
-            //line
-            raster_line(render_state->raster_ctx, lp0, lp1, COL_RED);
+        {
+            render_multicolor_cube(render_state, 15);
+            raster_line(render_state->raster_ctx,
+                        lp0, lp1, COL_RED);
         }
-        render_frame_end(render_state); 
+        render_frame_end(render_state);
+
     }
 
     render_state_destroy(render_state);
